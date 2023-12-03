fedcbafdaef35fe010106443de3e4f8816fb525e
hernan527 <hernan527@gmail.com>
2023-12-03 14:37:57 -0300

    segun el video de mena stack dockerizer2

--- front-prepagas/src/app/constants.ts
+++ front-prepagas/src/app/constants.ts
@@ -1,5 +1,7 @@
 // export const SERVER_URL = 'https://servidor-queplan.onrender.com';
-export const SERVER_URL = 'http://localhost:3000';
+// export const SERVER_URL = 'http://localhost:3000';
+export const SERVER_URL = 'http://vps-3785519-x.dattaweb.com:5000';
+
 // export const SERVER_URL = 'http://localhost:5200';
 // export const SERVER_URL = 'http://3.16.66.29:5200';
 // export const SERVER_URL = 'http://35.160.120.126';
