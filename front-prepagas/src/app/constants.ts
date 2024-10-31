760338f766cbfa67e9349b8b5a6beb17de3b548b
hernan527 <39295480+hernan527@users.noreply.github.com>
2024-10-31 06:43:04 -0300

    server_url

--- front-prepagas/src/app/constants.ts
+++ front-prepagas/src/app/constants.ts
@@ -1,7 +1,7 @@
 // export const SERVER_URL = 'https://servidor-queplan.onrender.com';
 // export const SERVER_URL = 'http://localhost:3000';
-
-export const SERVER_URL = 'http://localhost:5400';
+export const SERVER_URL = 'http://servicio.tuchat.com.ar';
+// export const SERVER_URL = 'http://localhost:5400';
 // export const SERVER_URL = 'http://3.16.66.29:5200';
 // export const SERVER_URL = 'http://35.160.120.126';
 // export const SERVER_URL = 'http://44.233.151.27';
