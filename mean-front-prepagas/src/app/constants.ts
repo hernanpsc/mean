1e6799f83db3bcea0dbc271e841f31ff4efff8ca
hernanpsc <136007081+hernanpsc@users.noreply.github.com>
2025-02-18 00:28:36 -0300

    Update constants.ts
    Update constants.ts

--- mean-front-prepagas/src/app/constants.ts
+++ mean-front-prepagas/src/app/constants.ts
@@ -1,2 +1,2 @@
-// export const SERVER_URL = 'http://servicio.tuchat.com.ar';
-export const SERVER_URL = 'http://localhost:5200';
+export const SERVER_URL = 'http://cotizador.tuchat.com.ar';
+// export const SERVER_URL = 'http://localhost:5200';
