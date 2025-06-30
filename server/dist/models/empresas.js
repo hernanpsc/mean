2f7cbc22d28b983a88699c25cd063f2ca81ce3ea
hernan527 <hernan527@gmail.com>
2025-06-30 00:00:26 -0300

    models

--- server/dist/models/empresas.js
+++ server/dist/models/empresas.js
@@ -4,7 +4,26 @@ const mongoose_1 = require("mongoose");
 const empresaSchema = new mongoose_1.Schema({
     name: {
         type: String,
-        required: true
+        required:true
+    },
+    item_id: {
+        type: Number,
+        required:true
+    },    
+    images: {
+        type: [String],
+    },
+    sigla: {
+        type: String,
+        required:true
+    },
+    lineas: {
+        type: [String]
+    },
+    factores: {
+      coeficiente: Number,
+      mono: [Number],
+      monotributo: {}
     }
 });
 const EmpresaModel = (0, mongoose_1.model)('empresas', empresaSchema);
